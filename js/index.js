7f89da3c513e36436fa94d9cf0eb3c0e6c8ff923
TahaSekk <sohailtaha128@gmail.com>
2022-07-29 02:24:36 +0500

    commti

--- js/index.js
+++ js/index.js
@@ -1,5 +1,24 @@
 let  CreateCard=()=>{
     let CardName =document.querySelector('.Name ').value;
     let inputUrl= document.querySelector('.Url').value;
-console.log(CardName);
+
+    // console.log(CardName,inputUrl);
+let CardObject=new Object();
+CardObject.Name=CardName;
+CardObject.ImageUrl=inputUrl;
+// console.log(CardObject)
+CardObject.Id=Math.floor(Math.random()*1000);
+CardObject.Points=0;
+// console.log(CardObject)
+let CardContainer = document.querySelector('.CardContainer');
+CardContainer.innerHTML = `
+<div class="card" style="width: 18rem;">
+<img src="${CardObject.ImageUrl}" class="card-img-top" alt="...">
+<div class="card-body">
+  <p>Name: ${CardObject.Name}</p>
+  <p>Points: ${CardObject.Points}</p>
+</div>
+    </div>
+`
+
 }
